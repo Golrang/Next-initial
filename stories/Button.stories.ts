4b2649b60af34d985318cae09c44ca35117f9111
amirHosseinKhademii <sourena2575@gmail.com>
2023-06-18 12:40:49 +0200

    Add sb to next

---
+++ stories/Button.stories.ts
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Button> = {
+  title: "Example/Button",
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {};
---
+++ stories/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Button = () => {
+  return (
+    <button
+      type="button"
+      className="bg-red-400 text-2xl text-white rounded p-4 dark:bg-blue-400 dark:text-black"
+    >
+      Button
+    </button>
+  );
+};
