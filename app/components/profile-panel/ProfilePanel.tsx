e26aaa8ead0b2c309aa0aac89c0993b6888f03db
sherinpoor.zahra <Zahra.Sherinpoor@Golrang.com>
2023-06-25 07:29:16 +0330

    create type

--- app/components/profile-panel/ProfilePanel.tsx
+++ app/components/profile-panel/ProfilePanel.tsx
@@ -1,8 +1,8 @@
-const ProfilePanelDetail = () => {
+const ProfilePanelDetail = ({name, position} : TProfilePanelDetail) => {
     return (
             <div className="flex flex-col space-y-4 justify-center items-center pb-4">
-                <span className="font-bold">Zahra Sherinpour</span>
-                <span className="text-gray-600">Front end developer at Golrang System</span>
+                <span className="font-bold">{name}</span>
+                <span className="text-gray-600">{position}</span>
             </div>
     )
 }
@@ -16,7 +16,7 @@ const ProfilePanelIntractionRow = () => {
     )
 }
 
-const ProfilePanelIntraction = () => {
+const ProfilePanelIntraction = ({} : TProfilePanelIntraction) => {
     return (
         <div className="flex flex-col space-y-6 justify-center border-b border-t p-5">
             <ProfilePanelIntractionRow />
@@ -25,7 +25,7 @@ const ProfilePanelIntraction = () => {
     )
 }
 
-const ProfilePanelMyItems = () => {
+const ProfilePanelMyItems = ({} :TProfilePanelMyItems) => {
     return (
         <div className="flex flex-col item-center">
             my items
@@ -33,12 +33,26 @@ const ProfilePanelMyItems = () => {
     )
 }
 
-export const ProfilePanel = () => {
+export const ProfilePanel = ({name, position, number_of_visit, number_of_immprestion, my_items}: TProfilePanel) => {
     return (
         <div className="rounded flex flex-col w-full h-auto border p-4">
-            <ProfilePanelDetail />
-            <ProfilePanelIntraction />
-            <ProfilePanelMyItems />
+            <ProfilePanelDetail name={name} position={position}/>
+            <ProfilePanelIntraction number_of_visit={number_of_visit} number_of_immprestion={number_of_immprestion} />
+            <ProfilePanelMyItems my_items={my_items}/>
         </div>
     )
 }
+
+type TProfilePanel ={
+    name: string,
+    position : string,
+    number_of_visit : number,
+    number_of_immprestion : number,
+    my_items : string
+}
+
+type TProfilePanelDetail = Pick<TProfilePanel, "name" | "position">
+type TProfilePanelIntraction = Pick<TProfilePanel, "number_of_visit" | "number_of_immprestion">
+type TProfilePanelMyItems = Pick<TProfilePanel, "my_items">
+
+  
