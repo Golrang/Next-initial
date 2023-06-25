489f61939f0f16d944b72de949b2d45a25e14056
amirHosseinKhademii <sourena2575@gmail.com>
2023-06-25 13:50:10 +0200

    Add react query as hydrate state

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,11 +3,10 @@ import React from "react";
 import { ProfilePanel } from "../components/profile-panel";
 
 const Page = () => {
-  
   return (
     <div className=" grid grid-cols-4 p-6">
       <div className="col-span-1">
-      <ProfilePanelContainer />
+        <ProfilePanelContainer />
       </div>
       <div className="col-span-2"></div>
       <div className="col-span-1  flex flex-col">
@@ -34,21 +33,22 @@ const SubscriptionPanelContainer = async () => {
 
 const ProfilePanelContainer = async () => {
   const result = await getData("https://jsonplaceholder.typicode.com/users/1");
-  console.log(result)
+
   return (
-    <ProfilePanel name={result.name} position={result.email} number_of_visit={result.company.name.length} number_of_impression={result.phone.length} my_items={result.website} />
+    <ProfilePanel
+      name={result.name}
+      position={result.email}
+      number_of_visit={result.company.name.length}
+      number_of_impression={result.phone.length}
+      my_items={result.website}
+    />
   );
 };
 
-
-const getData = async (url: string) => {
+export const getData = async (url: string) => {
   const res = await fetch(url);
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
 
