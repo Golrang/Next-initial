a28b81aeee80ac03a341a2cecaa72f8f9cf986ed
Niloofar <Khademi.Niloufar@Golrang.com>
2023-06-28 14:15:57 +0330

    linkedIn post has been added

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,6 @@
 import { SubscriptionPanel } from "@/app/components";
 import React from "react";
+import { Post } from "../components/post";
 
 export const preload = (url: string) => {
   // void evaluates the given expression and returns undefined
@@ -11,7 +12,9 @@ const Page = () => {
   return (
     <div className=" grid grid-cols-4 p-6">
       <div className="col-span-1"></div>
-      <div className="col-span-2"></div>
+      <div className="col-span-2">
+        <Post />
+      </div>
       <div className="col-span-1  flex flex-col">
         <div className="flex flex-col w-full h-80" />
         <SubscriptionPanelContainer />
---
+++ stories/Post.stories.tsx
@@ -0,0 +1,17 @@
+import {Post} from "../app/components/post";
+import type {Meta, StoryObj} from "@storybook/react";
+
+const meta: Meta<typeof Post> = {
+  title: "Components/Post",
+  component: Post,
+};
+
+export default meta;
+type Story = StoryObj<typeof Post>;
+
+export const Primary: Story = {
+  args: {
+    title: "Title",
+    description: "Description",
+  },
+};
